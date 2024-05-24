cf5ed7aa9f66801129268a2543af4a241c17ff91
Preethiviraj <20eumt057@skcet.ac.in>
2024-05-24 17:50:22 +0530

    Post Development changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'
 import  { server,app } from './socket/socket.js'
@@ -6,6 +7,8 @@ import  { server,app } from './socket/socket.js'
 
 dotenv.config()
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
@@ -20,6 +23,12 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname, 'frontend' ,"dist", 'index.html'))
+})
  
 
 server.listen(PORT ,()=>{ 
